0d87bf2242a879e6b788199a95d5eb1e26923d8a
ZAYKA1234 <152148530+ZAYKA1234@users.noreply.github.com>
2024-10-03 11:59:06 +0500

    new func

--- state.py
+++ state.py
@@ -1,10 +1,12 @@
 class BotState:
     """Класс для управления состоянием бота"""
     def __init__(self):
+        self.waiting_for_domen = False
         self.is_started = False
 
     def reset(self):
         """Сбрасываем состояния бота"""
+        self.waiting_for_domen = False
         self.is_started = False
 
 # Экземпляр для управления состоянием
